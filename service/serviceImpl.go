4056cfe61907372a459a6d04017dc8379d57f71e
Ramashankar Kumar <ramashankar.kumar@gridinfocom.com>
2022-09-27 18:11:58 +0530

    Refactor Code

--- service/serviceImpl.go
+++ service/serviceImpl.go
@@ -7,9 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"net/smtp"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/unidoc/unipdf/v3/common/license"
@@ -53,7 +51,6 @@ func (e *Connection) Connect() {
 
 func (e *Connection) SendEmail(emailData pojo.EmailModel) (string, error) {
 
-	//	err := sendMail2(emailData)
 	err := sendMailWithAttachment(emailData)
 
 	if err != nil {
@@ -84,63 +81,6 @@ func (*Connection) newMethod(emailData pojo.EmailModel) error {
 		return errors.New("Unable To Insert New Record")
 	}
 	return err
-}
-
-func mail(emailData pojo.EmailPojo) error {
-	from := "ranveer.singh@gridinfocom.com"
-	password := "xsmtpsib-2af236a5040e4b54343f4fe5b59826e9e7588b2e33d160249ab60a3060fbf348-LAnNRJT0sxHcPqYr"
-
-	toEmailAddress := emailData.EmailTo
-	host := "smtp-relay.sendinblue.com"
-	port := "587"
-	address := host + ":" + port
-	msg := ComposeMsg(emailData)
-
-	auth := smtp.PlainAuth("", from, password, host)
-	fmt.Println(auth)
-
-	err := smtp.SendMail(address, auth, from, toEmailAddress, []byte(msg))
-	return err
-}
-
-func ComposeMsg(emailData pojo.EmailPojo) string {
-
-	// empty string
-	msg := ""
-	// set sender
-	msg += fmt.Sprintf("From: %s\r\n", emailData.EmailTo)
-	// if more than 1 recipient
-	if len(emailData.EmailTo) > 0 {
-		msg += fmt.Sprintf("Cc: %s\r\n", strings.Join(emailData.EmailCC, ";"))
-	}
-	// add subject
-	msg += fmt.Sprintf("Subject: %s\r\n", emailData.EmailSubject)
-	// add mail body
-	msg += fmt.Sprintf("\r\n%s\r\n", emailData.EmailBody)
-	return msg
-}
-
-func sendMail2(emailData pojo.EmailPojo) error {
-	m := gomail.NewMessage()
-	m.SetHeaders(map[string][]string{
-		"From":    {m.FormatAddress("ramashankar.kumar@gridinfocom.com", "Ramashankar")},
-		"To":      emailData.EmailTo,
-		"Cc":      emailData.EmailCC,
-		"Subject": emailData.EmailSubject,
-	})
-	//Directory and fileName
-	//	m.Attach("data/download/searchResult2_57_44_pm.pdf")
-	m.SetBody("text/plain", emailData.EmailBody)
-	// Settings for SMTP server
-	d := gomail.NewDialer("smtp-relay.sendinblue.com", 587, "ramashankar.kumar@gridinfocom.com", "vrDYSBXaFb2y6VnE")
-
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-	// Now send E-Mail
-	if err := d.DialAndSend(m); err != nil {
-		fmt.Println(err)
-		return err
-	}
-	return nil
 }
 
 func (e *Connection) SearchFilter(search pojo.Search) ([]*pojo.EmailPojo, error) {
